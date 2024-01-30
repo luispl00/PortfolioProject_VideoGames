f8ff94be76cc653a98b8d7d5d61f27fa8575f447
Luis Antonio Castillo Plancarte <137434226+luispl00@users.noreply.github.com>
2024-01-30 10:09:13 -0600

    Update 1. GoldenEra_analysis.sql

--- 1. GoldenEra_analysis.sql
+++ 1. GoldenEra_analysis.sql
@@ -1,23 +1,21 @@
 -- THE GOLDEN ERA OF VIDEO GAMES
+
 /* Analysis of the years available in our data set to answer the question: 
 When was the golden age of video games?
+
+Skills needed: Filtering with having, CTEs, Joins, Aggregate functions, Complex Joins (CTEs with DB tables), Set operations
 */
-
-
 
 SELECT *
 FROM game_sales
-
+	
 SELECT *
 FROM game_reviews
 
 -- Let's begin by looking at some of the top selling video games of all time
-
 SELECT TOP 10 *
 FROM game_sales
 ORDER BY games_sold DESC
-
-
 
 /*
 The best-selling video games were released between 1985 to 2017! That's quite a range
@@ -25,9 +23,7 @@ We'll have to use data from the reviews table to gain more insight on the best y
 */
 
 
-
 -- One big shortcoming is that there is not any reviews data for some of the games on the game_sales table.
-
 SELECT COUNT(*) AS NoReviews_Count
 FROM game_sales AS s
 LEFT JOIN game_reviews AS r
@@ -41,7 +37,6 @@ to keep in mind as we move on to evaluating results from more sophisticated quer
 */
 
 
-
 -- There are lots of ways to measure the best years for video games! Let's start with what the critics think.
 SELECT TOP 10 year, ROUND(AVG(critic_score), 2) AS avg_critic_score
 FROM game_sales AS s
@@ -51,7 +46,7 @@ LEFT JOIN game_reviews AS r
 WHERE critic_score IS NOT NULL
 GROUP BY year
 ORDER by avg_critic_score DESC
-
+	
 /*
 The range of great years according to critic reviews goes from 1982 until 2020.
 We are no closer to finding the golden age of video games!
@@ -59,7 +54,6 @@ Some of those avg_critic_score values look like suspiciously round numbers for a
 The value for 1982, 1994 and 1992 looks especially suspicious. 
 Maybe there weren't a lot of video games in our dataset that were released in certain years.
 */
-
 
 
 -- Based on the previuos query, let's see how many games we have per year
@@ -78,8 +72,7 @@ that's why we have round numbers on the averegar_critic_score.
 */
 
 
-
--- Let's filter our query with just years that have 5 or more records in their group (where critic_score is not null).
+-- Let's filter our query with just years that have 5 or more records in their group.
 SELECT year, ROUND(AVG(critic_score), 2) AS avg_critic_score, COUNT(*) AS num_games
 FROM game_sales AS s
 LEFT JOIN game_reviews AS r
@@ -132,7 +125,6 @@ consideration as the golden age of video games based on critic_score alone
 */
 
 
-
 -- Let's move on to looking at the opinions of another important group of people: players. 
 SELECT TOP 10 year, ROUND(AVG(user_score), 2) AS avg_user_score, COUNT(*) AS num_games
 FROM game_sales AS s
@@ -146,7 +138,6 @@ ORDER by avg_user_score DESC
 /*
 We've got a list of the top ten years according to both critic reviews and user reviews.
 */
-
 
 
 -- Let's find out which years were loved by critics and players
@@ -179,5 +170,5 @@ INNER JOIN top_user_years AS u
 ON c.year = u.year
 
 /*
-4 years have in common user and critics
+4 years have in common user and critics.
 */
